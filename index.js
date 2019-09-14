84ba89fb9ba28a42b5d1c71db5a2f601bd18062b
choephix <i@thechoephix.com>
2019-09-14 19:51:34 +0000

    try/catch

--- index.js
+++ index.js
@@ -20,28 +20,35 @@ const fetch = require("node-fetch")
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
 
 async function updateDomainIP() {
-  const DUCKDNS_DOMAINS = process.env.DUCKDNS_DOMAINS
-  const DUCKDNS_TOKEN = process.env.DUCKDNS_TOKEN
-  const DUCKDNS_IPADDRESS = process.env.DUCKDNS_IPADDRESS
-  const response = await fetch(`https://www.duckdns.org/update?domains=${DUCKDNS_DOMAINS}&token=${DUCKDNS_TOKEN}&ip=${DUCKDNS_IPADDRESS}&verbose=true`)
-  console.log( await response.text() )
+  try {
+    const DUCKDNS_DOMAINS = process.env.DUCKDNS_DOMAINS
+    const DUCKDNS_TOKEN = process.env.DUCKDNS_TOKEN
+    const DUCKDNS_IPADDRESS = process.env.DUCKDNS_IPADDRESS
+    const response = await fetch(`https://www.duckdns.org/update?domains=${DUCKDNS_DOMAINS}&`+
+                                 `token=${DUCKDNS_TOKEN}&ip=${DUCKDNS_IPADDRESS}&verbose=true`)
+    console.log( await response.text() )
+  }
+  catch( e ) { console.error( e ) }
 }
 
 async function loop() {
   while( true )
   {
     await sleep(1000)
-    const response = await fetch(PING_URL)
-    const pingtime = parseInt( await response.text() )
-    const pingdelta = Math.floor( .001 * (new Date().getTime() - pingtime) )
-    if ( VERBOSE )
-      console.log( `\x1b[${pingdelta>.9?93:94};1m${pingdelta}\x1b[0;94m seconds since last ping\x1b[0m` )
-    if ( pingdelta > MAX_PING_DELTA )
-    {
-      console.log("That's it! i'm killing the vm...")
-      await stopinstance()
-      return
+    try {
+      const response = await fetch(PING_URL)
+      const pingtime = parseInt( await response.text() )
+      const pingdelta = Math.floor( .001 * (new Date().getTime() - pingtime) )
+      if ( VERBOSE )
+        console.log( `\x1b[${pingdelta>.9?93:94};1m${pingdelta}\x1b[0;94m seconds since last ping\x1b[0m` )
+      if ( pingdelta > MAX_PING_DELTA )
+      {
+        console.log("That's it! i'm killing the vm...")
+        await stopinstance()
+        return
+      }
     }
+    catch( e ) { console.error( e ) }
   }
 }
 
